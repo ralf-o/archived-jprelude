76bfbcaf331067064acfb9b56ba45fa3f835710d
ralf-o <ralf-o@users.noreply.github.com>
2015-04-07 03:42:41 +0200

    Update

--- jprelude/common/src/main/java/org/jprelude/common/csv/CsvRowHandler.java
+++
@@ -1,9 +0,0 @@
-package org.jprelude.common.csv;
-
-public interface CsvRowHandler {
-    void set(String fieldName, Object fieldValue);
-    void set(int fieldIdx, Object fieldValue);
-    void add(Object... fields);
-    void newRow();
-    int rowNumber();
-}
